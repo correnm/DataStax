98a4af88adc8f60aca99b669cc0d02c9d5a43a42
John Reddy <john.reddy@g2-ops.com>
2017-05-25 23:00:17 -0400

    Make Class Serializable and Other Changes

--- washington/src/main/java/com/g2ops/washington/utils/DatabaseConnectionManager.java
+++ washington/src/main/java/com/g2ops/washington/utils/DatabaseConnectionManager.java
@@ -1,26 +1,32 @@
 package com.g2ops.washington.utils;
 
+import java.io.Serializable;
+
 import com.datastax.driver.core.AuthProvider;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.PlainTextAuthProvider;
 import com.datastax.driver.core.Session;
 
-public class DatabaseConnectionManager {
+public class DatabaseConnectionManager implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private String USERNAME; 
 	private String PASSWORD; 
 	private String[] CONTACT_POINTS;
 	private int PORT;
+	private String KEYSPACE;
 
 	private Cluster cluster;
 	private Session session;
 
-	public DatabaseConnectionManager(String uName, String pWord, String[] cPoints, String port) {
+	public DatabaseConnectionManager(String uName, String pWord, String[] cPoints, String port, String keyspace) {
 
 		this.USERNAME = uName;
 		this.PASSWORD = pWord;
 		this.CONTACT_POINTS = cPoints;
 		this.PORT = Integer.parseInt(port);
+		this.KEYSPACE = keyspace;
 
 		AuthProvider authProvider = new PlainTextAuthProvider(this.USERNAME, this.PASSWORD);
 
@@ -32,7 +38,7 @@ public class DatabaseConnectionManager {
 				.build();
 
 		// The Session is what you use to execute queries. Likewise, it is thread-safe and should be reused.
-		session = cluster.connect();
+		session = cluster.connect(this.KEYSPACE);
 
 	}
 
@@ -40,8 +46,16 @@ public class DatabaseConnectionManager {
 		return session;    	
 	}
 
+	public void closeSession() {
+		if (session != null && !session.isClosed()) {
+			session.close();
+		}
+	}
+
 	public void closeConnection() {
-		cluster.close();
+		if (cluster != null && !cluster.isClosed()) {
+			cluster.close();
+		}
 	}
 
 }
