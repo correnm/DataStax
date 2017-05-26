4f877805cc92bc7c191261b4e67c6e8bdf02f5c2
John Reddy <john.reddy@g2-ops.com>
2017-05-25 22:57:30 -0400

    Make Class Serializable

--- washington/src/main/java/com/g2ops/washington/types/OrgUnit.java
+++ washington/src/main/java/com/g2ops/washington/types/OrgUnit.java
@@ -1,6 +1,10 @@
 package com.g2ops.washington.types;
 
-public class OrgUnit {
+import java.io.Serializable;
+
+public class OrgUnit implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private String orgUnitCode, orgUnitName;
 	
