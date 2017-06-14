f48819928fca43eca6f47ed97f81b9707c0c16b7
John Reddy <john.reddy@g2-ops.com>
2017-06-13 22:33:25 -0400

    Added getOrgDBSession Method

--- washington/src/main/java/com/g2ops/washington/utils/SessionUtils.java
+++ washington/src/main/java/com/g2ops/washington/utils/SessionUtils.java
@@ -1,6 +1,10 @@
 package com.g2ops.washington.utils;
 
+import com.datastax.driver.core.Session;
+import com.g2ops.washington.utils.DatabaseConnectionManager;
+import java.util.Map;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -16,6 +20,33 @@ public class SessionUtils {
 				.getExternalContext().getRequest();
 	}
 
+	public static Session getOrgDBSession() {
+
+		// get the servlet's context
+		ServletContext ctx = getRequest().getServletContext();
+
+		// get the user's session
+		HttpSession userSession = getSession();
+
+		// get the orgKeyspace from the session
+		String orgKeyspace = (String)userSession.getAttribute("orgKeyspace");
+
+		// get the Hash Map of the Organization database connections
+		@SuppressWarnings("unchecked")
+		Map<String, DatabaseConnectionManager> DBConnectionsHashMap = (Map<String, DatabaseConnectionManager>) ctx.getAttribute("OrgDBConnections");
+		
+		// get the database connection for this user's organization
+		DatabaseConnectionManager orgDBConnection = DBConnectionsHashMap.get(orgKeyspace);
+		
+		// get the database connection session
+		Session DBSession = orgDBConnection.getSession();
+		
+		// return the Org's DB Session
+		return DBSession;
+
+	}
+
+	/*
 	public static String getOrgKeyspace() {
 		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
 				.getExternalContext().getSession(false);
@@ -33,5 +64,6 @@ public class SessionUtils {
 				.getExternalContext().getSession(false);
 		return session.getAttribute("userRole").toString();
 	}
-
+	*/
+	
 }
