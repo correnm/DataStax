42f2cc78c98e713bfe98377019c832693e36b254
John Reddy <john.reddy@g2-ops.com>
2017-06-21 23:56:03 -0400

    Business Value Attribution Page Bean

---
+++ washington/src/main/java/com/g2ops/washington/beans/BusinessPracticeTable.java
@@ -0,0 +1,59 @@
+package com.g2ops.washington.beans;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.g2ops.washington.types.BusinessPractice;
+import com.g2ops.washington.utils.DatabaseQueryService;
+
+@ManagedBean
+@ViewScoped
+public class BusinessPracticeTable {
+
+	private DatabaseQueryService dqs = new DatabaseQueryService();
+	private ResultSet rs;
+	private Iterator<Row> iterator;
+	private BusinessPractice biz;
+	private List<BusinessPractice> bizList = new ArrayList<BusinessPractice>();
+	
+	public List<BusinessPractice> getBusinessData() {
+		// clear out any old content before retrieving new database info
+		bizList.clear();
+		
+		// execute the query against the table
+		populateBusinessData();
+
+		iterator = rs.iterator();
+
+		//iterate over the results. 
+		while (iterator.hasNext()) {
+			Row row = iterator.next();
+
+			biz = new BusinessPractice(row.getString("category"), 
+					row.getString("business_value"), 
+					row.getDecimal("collateral_damage_current"), 
+					row.getDecimal("target_distribution_current"), 
+					row.getDecimal("confidentiality_req_current"), 
+					row.getDecimal("integrity_req_current"), 
+					row.getDecimal("availability_req_current"));
+			// Save each database record to the list.
+			bizList.add(biz);
+		}
+		
+		return bizList;
+
+	}
+
+	private void populateBusinessData () {
+
+		rs = dqs.RunQuery("select category, business_value,collateral_damage_current, target_distribution_current, confidentiality_req_current, integrity_req_current, availability_req_current from business_practice");
+
+	}
+
+}
