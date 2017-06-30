2164d46cf44553bdff743f0579054ce9bc5f6dfa
John Reddy <john.reddy@g2-ops.com>
2017-06-29 22:48:21 -0400

    Getting Sites from DB

--- washington/src/main/java/com/g2ops/washington/beans/SiteSelectOneListboxBean.java
+++ washington/src/main/java/com/g2ops/washington/beans/SiteSelectOneListboxBean.java
@@ -3,12 +3,18 @@ package com.g2ops.washington.beans;
 import java.io.Serializable;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.g2ops.washington.types.OrgUnit;
 import com.g2ops.washington.types.Site;
+import com.g2ops.washington.utils.SessionUtils;
 
 @ManagedBean
 @SessionScoped
@@ -21,11 +27,23 @@ public class SiteSelectOneListboxBean implements Serializable {
 	
 	public SiteSelectOneListboxBean() {
 		
+		Session dbSession = SessionUtils.getOrgDBSession();
+		ResultSet rs;
+		Row row;
+		Iterator<Row> iterator;
 		this.sites = new ArrayList<Site>();
-		this.sites.add(new Site("1", "Virginia Beach Office"));
-		this.sites.add(new Site("2", "San Diego Office"));
-		this.sites.add(new Site("3", "Tampa Bay Office in Florida"));
+
+		// execute the query
+		rs = dbSession.execute("select site_id, site_name from sites");
+
+		iterator = rs.iterator();
 		
+		while (iterator.hasNext()) {
+			row = iterator.next();
+			this.sites.add(new Site(row.getUUID("site_id").toString(), row.getString("site_name")));
+			System.out.println("site_name: " + row.getString("site_name"));
+		}
+
 	}
 	
 	public List<Site> getSites() {
