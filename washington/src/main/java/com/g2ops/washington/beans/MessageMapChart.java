b061325ad759443b2e0809b8d9b51fbb39d0cdbf
John Reddy <john.reddy@g2-ops.com>
2017-07-24 22:56:07 -0400

    Fixed Space in Filepath Bug

--- washington/src/main/java/com/g2ops/washington/beans/MessageMapChart.java
+++ washington/src/main/java/com/g2ops/washington/beans/MessageMapChart.java
@@ -1,7 +1,8 @@
 package com.g2ops.washington.beans;
 
 import java.io.File;
-
+import java.net.URL;
+import java.net.URLDecoder;
 import java.util.Scanner;
 
 import javax.faces.bean.ManagedBean;
@@ -10,6 +11,23 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import com.g2ops.washington.utils.FusionCharts;
+
+/**
+ * @author 		Dang Le, G2 Ops, Virginia Beach, VA
+ * @version 	1.00, May 2017
+ * @see			https://stackoverflow.com/questions/32251251/java-classloader-getresource-with-special-characters-in-path
+ * @exception
+ * 
+ * <p>Known Bugs: (a list of bugs and other problems)
+ *
+ *
+ * <p>Revision History:
+ * Date				Author				Revision Description
+ * 24-Jul-2017		corren.mccoy		Changed file access pattern to properly handle getResource with special characters in the path
+ * 
+ * 
+ * 
+ */
 
 @ManagedBean
 public class MessageMapChart {
@@ -26,7 +44,13 @@ public class MessageMapChart {
  
         	StringBuilder result = new StringBuilder("");
         	ClassLoader classLoader = getClass().getClassLoader();
-        	File file = new File(classLoader.getResource("json/messagemap.json").getFile());
+        	//File file = new File(classLoader.getResource("json/messagemap.json").getFile());
+        	URL resource = classLoader.getResource("json/messagemap.json");
+        	// 07-24-2017: handle special characters which might be contained in the path.
+        	// Without special handling, can result in java.io.FileNotFoundException at runtime
+        	// which can cause any screen where this chart is embedded not to display.
+        	String configPath = URLDecoder.decode(resource.getFile(), "UTF-8");
+        	File file = new File(configPath);
         	Scanner scanner = new Scanner(file);
 
         	while (scanner.hasNextLine()) {
@@ -41,6 +65,7 @@ public class MessageMapChart {
             JSONObject jsonObject = (JSONObject) obj;
             chartData = jsonObject.toJSONString();
 
+            // DEBUG: print chart data to console
     		//System.out.println(chartData);
   
         } catch (Exception e) {
@@ -48,11 +73,11 @@ public class MessageMapChart {
         }
 		
 		messageMap = new FusionCharts(
-                "dragnode",// chartType
-                "messageMapID",// chartId
-                "100%","100%",// chartWidth, chartHeight
-                "MessageMapContainer",// chartContainer
-                "json",// dataFormat
+                "dragnode",				// chartType
+                "messageMapID",			// chartId
+                "100%","100%",			// chartWidth, chartHeight
+                "MessageMapContainer",	// chartContainer
+                "json",					// dataFormat
                 chartData
 		);
     	data = messageMap.render();
