1a83f4a8e61b72440f0eaa861ee2cb4c6924c296
John Reddy <john.reddy@g2-ops.com>
2017-07-24 22:56:50 -0400

    Fixed Space in Filepath Bug

--- washington/src/main/java/com/g2ops/washington/beans/TimelineManagementChart.java
+++ washington/src/main/java/com/g2ops/washington/beans/TimelineManagementChart.java
@@ -1,7 +1,8 @@
 package com.g2ops.washington.beans;
 
 import java.io.File;
-
+import java.net.URL;
+import java.net.URLDecoder;
 import java.util.Scanner;
 
 import javax.faces.bean.ManagedBean;
@@ -10,6 +11,22 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import com.g2ops.washington.utils.FusionCharts;
 
+/**
+ * @author 		Dang Le, G2 Ops, Virginia Beach, VA
+ * @version 	1.00, May 2017
+ * @see			https://stackoverflow.com/questions/32251251/java-classloader-getresource-with-special-characters-in-path
+ * @exception
+ * 
+ * <p>Known Bugs: (a list of bugs and other problems)
+ *
+ *
+ * <p>Revision History:
+ * Date				Author				Revision Description
+ * 24-Jul-2017		corren.mccoy		Changed file access pattern to properly handle getResource with special characters in the path
+ * 
+ * 
+ * 
+ */
 	@ManagedBean
 	public class TimelineManagementChart {
 	   
@@ -25,9 +42,16 @@ import com.g2ops.washington.utils.FusionCharts;
 	        	
 	        	StringBuilder result = new StringBuilder("");
 	        	ClassLoader classLoader = getClass().getClassLoader();
-	        	File file = new File(classLoader.getResource("json/timeline.json").getFile());
-	        	Scanner scanner = new Scanner(file);
-
+	        	// File file = new File(classLoader.getResource("json/timeline.json").getFile());
+	        	URL resource = classLoader.getResource("json/timeline.json");
+	        	// 07-24-2017: handle special characters which might be contained in the path.
+	        	// Without special handling, can result in java.io.FileNotFoundException at runtime
+	        	// which can cause any screen where this chart is embedded not to display.
+	        	String configPath = URLDecoder.decode(resource.getFile(), "UTF-8");
+	        	File file = new File(configPath);
+	        	Scanner scanner = new Scanner(file);	        	
+       	
+	        	
 	        	while (scanner.hasNextLine()) {
 	    			String line = scanner.nextLine();
 	    			result.append(line).append("\n");
@@ -40,6 +64,7 @@ import com.g2ops.washington.utils.FusionCharts;
 	            JSONObject jsonObject = (JSONObject) obj;
 	            chartData = jsonObject.toJSONString();
 
+	            // DEBUG: print chart data to the console
 	    		//System.out.println(chartData);
 	 
 	        } catch (Exception e) {
@@ -47,11 +72,11 @@ import com.g2ops.washington.utils.FusionCharts;
 	        }
 
 	        timelineManagementChart = new FusionCharts(
-                    "sparkcolumn",// chartType
-                    "timelineChartID",// chartId
-                    "100%","100%",// chartWidth, chartHeight
-                    "timelineChartContainer",// chartContainer
-                    "json",// dataFormat
+                    "sparkcolumn",				// chartType
+                    "timelineChartID",			// chartId
+                    "100%","100%",				// chartWidth, chartHeight
+                    "timelineChartContainer",	// chartContainer
+                    "json",						// dataFormat
                     chartData
     		);
 	    	data = timelineManagementChart.render();
