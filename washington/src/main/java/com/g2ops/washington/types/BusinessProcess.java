8b942a9b11f7f28028b7d682ab755aaea937e0bc
John Reddy <john.reddy@g2-ops.com>
2017-08-14 21:13:33 -0400

    Added Data Field

--- washington/src/main/java/com/g2ops/washington/types/BusinessProcess.java
+++ washington/src/main/java/com/g2ops/washington/types/BusinessProcess.java
@@ -2,6 +2,21 @@ package com.g2ops.washington.types;
 
 import java.io.Serializable;
 
+/**
+ * @author 		John Reddy, G2 Ops, Virginia Beach, VA
+ * @version 	1.00, May 2017
+ * @see			
+ * @exception
+ * 
+ * <p>Known Bugs: (a list of bugs and other problems)
+ *
+ *
+ * <p>Revision History:
+ * Date				Author				Revision Description
+ * 14-Aug-2017		corren.mccoy		Added new column for annual revenue
+ * 
+ */
+
 public class BusinessProcess implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -13,8 +28,12 @@ public class BusinessProcess implements Serializable {
 	private String darkWebCost;
 	private String nivScore;
 	private String cyvarValue;
+	private String annualRevenue;
 
-	public BusinessProcess (String processName, String recordType, String numRecords, String breachCost, String darkWebCost, String nivScore, String cyvarValue) {
+	public BusinessProcess (String processName, String recordType, String numRecords, 
+			String breachCost, String darkWebCost, String nivScore, String cyvarValue, 
+			String annualRevenue) 
+	{
 		this.processName = processName;
 		this.recordType = recordType;
 		this.numRecords = numRecords;
@@ -22,6 +41,7 @@ public class BusinessProcess implements Serializable {
 		this.darkWebCost = darkWebCost;
 		this.nivScore = nivScore;
 		this.cyvarValue = cyvarValue;
+		this.annualRevenue = annualRevenue;
 	}
 	
 	public String getProcessName() {
@@ -51,6 +71,8 @@ public class BusinessProcess implements Serializable {
 	public String getCyvarValue() {
 		return cyvarValue;
 	}
-		
+	public String getAnnualRevenue() {
+		return annualRevenue;
+	}	
 }
 
