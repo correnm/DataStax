e5097a79eccc8a95f547c1d2bd1d4d53b2722854
John Reddy <john.reddy@g2-ops.com>
2017-08-14 21:16:08 -0400

    Adjusted Test Data and Made Tolerance Line Increase Over Time

--- washington/src/main/java/com/g2ops/washington/beans/OrgRiskHistoryLineChart.java
+++ washington/src/main/java/com/g2ops/washington/beans/OrgRiskHistoryLineChart.java
@@ -3,6 +3,21 @@ package com.g2ops.washington.beans;
 import javax.faces.bean.*;
 import com.g2ops.washington.utils.FusionCharts;
 
+/**
+ * @author 		John Reddy, G2 Ops, Virginia Beach, VA
+ * @version 	1.00, May 2017
+ * @see			
+ * @exception
+ * 
+ * <p>Known Bugs: (a list of bugs and other problems)
+ *
+ *
+ * <p>Revision History:
+ * Date				Author				Revision Description
+ * 14-Aug-2017		corren.mccoy		Moved timeline ahead by one month for demo purposes
+ * 
+ */
+
 @ManagedBean
 public class OrgRiskHistoryLineChart {
 
@@ -23,10 +38,9 @@ public class OrgRiskHistoryLineChart {
 		chartData = chartData.concat("\"paletteColors\": \"#28637e\",");
 		chartData = chartData.concat("},");
 		chartData = chartData.concat("\"data\": [");
-		chartData = chartData.concat("{\"label\": \"7/15\",\"value\": \"50000000\"},");
 		chartData = chartData.concat("{\"label\": \"8/15\",\"value\": \"45000000\"},");
 		chartData = chartData.concat("{\"label\": \"9/15\",\"value\": \"60000000\"},");
-		chartData = chartData.concat("{\"label\": \"10/15\",\"value\": \"75000000\"},");
+		chartData = chartData.concat("{\"label\": \"10/15\",\"value\": \"51000000\"},");
 		chartData = chartData.concat("{\"label\": \"11/15\",\"value\": \"65000000\"},");
 		chartData = chartData.concat("{\"label\": \"12/15\",\"value\": \"55000000\"},");
 		chartData = chartData.concat("{\"label\": \"1/16\",\"value\": \"50000000\"},");
@@ -37,8 +51,8 @@ public class OrgRiskHistoryLineChart {
 		chartData = chartData.concat("{\"label\": \"6/16\",\"value\": \"35000000\"},");
 		chartData = chartData.concat("{\"label\": \"7/16\",\"value\": \"30000000\"},");
 		chartData = chartData.concat("{\"label\": \"8/16\",\"value\": \"45000000\"},");
-		chartData = chartData.concat("{\"label\": \"9/16\",\"value\": \"50000000\"},");
-		chartData = chartData.concat("{\"label\": \"10/16\",\"value\": \"40000000\"},");
+		chartData = chartData.concat("{\"label\": \"9/16\",\"value\": \"72000000\",\"toolText\": \"Global Security Event: Petya\"},");
+		chartData = chartData.concat("{\"label\": \"10/16\",\"value\":\"40000000\",\"toolText\": \"Petya Mitigation\"},");
 		chartData = chartData.concat("{\"label\": \"11/16\",\"value\": \"35000000\"},");
 		chartData = chartData.concat("{\"label\": \"12/16\",\"value\": \"25000000\"},");
 		chartData = chartData.concat("{\"label\": \"1/17\",\"value\": \"20000000\"},");
@@ -46,11 +60,13 @@ public class OrgRiskHistoryLineChart {
 		chartData = chartData.concat("{\"label\": \"3/17\",\"value\": \"75000000\",\"toolText\": \"Global Security Event: WannaCry\"},");
 		chartData = chartData.concat("{\"label\": \"4/17\",\"value\": \"30000000\",\"toolText\": \"WannaCry Mitigation\"},");
 		chartData = chartData.concat("{\"label\": \"5/17\",\"value\": \"25000000\"},");
-		chartData = chartData.concat("{\"label\": \"6/17\",\"value\": \"20000000\"}");
+		chartData = chartData.concat("{\"label\": \"6/17\",\"value\": \"22000000\"},");
+		chartData = chartData.concat("{\"label\": \"7/17\",\"value\": \"20000000\"}");
 		chartData = chartData.concat("],");
 		chartData = chartData.concat("\"trendlines\": [{");
 		chartData = chartData.concat("\"line\": [{");
-		chartData = chartData.concat("\"startvalue\": \"50000000\",");
+		chartData = chartData.concat("\"startvalue\": \"42000000\",");
+		chartData = chartData.concat("\"endvalue\": \"50000000\",");
 		chartData = chartData.concat("\"color\": \"#595a5c\",");
 		chartData = chartData.concat("\"displayvalue\": \"Risk{br}Tolerance{br}$50M\",");
 		chartData = chartData.concat("\"valueOnRight\": \"1\",");
