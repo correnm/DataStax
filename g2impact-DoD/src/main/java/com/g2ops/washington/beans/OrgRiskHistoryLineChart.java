9746973a48557bbf5ced0e62135890c31df9a89f
Corren McCoy <corren.mccoy@g2-ops.com>
2017-08-28 12:09:09 -0400

    Change in mission impact threshold data representation

--- g2impact-DoD/src/main/java/com/g2ops/washington/beans/OrgRiskHistoryLineChart.java
+++ g2impact-DoD/src/main/java/com/g2ops/washington/beans/OrgRiskHistoryLineChart.java
@@ -29,7 +29,7 @@ public class OrgRiskHistoryLineChart {
 
 		chartData = "{\"chart\": {";
 		chartData = chartData.concat("\"theme\": \"g2ops\",");
-		chartData = chartData.concat("\"caption\": \"Consolidated Risk Likelihood\",");
+		chartData = chartData.concat("\"caption\": \"Consolidated Mission Impact Likelihood\",");
 		chartData = chartData.concat("\"xAxisName\": \"\",");
 		chartData = chartData.concat("\"yAxisName\": \"\",");
 		chartData = chartData.concat("\"lineThickness\": \"2\",");
@@ -38,8 +38,8 @@ public class OrgRiskHistoryLineChart {
 		chartData = chartData.concat("\"paletteColors\": \"#28637e\",");
 		chartData = chartData.concat("},");
 		chartData = chartData.concat("\"data\": [");
-		chartData = chartData.concat("{\"label\": \"8/15\",\"value\": \"75.3\"},");
-		chartData = chartData.concat("{\"label\": \"9/15\",\"value\": \"60.0\"},");
+		chartData = chartData.concat("{\"label\": \"8/15\",\"value\": \"55.3\"},");
+		chartData = chartData.concat("{\"label\": \"9/15\",\"value\": \"54.0\"},");
 		chartData = chartData.concat("{\"label\": \"10/15\",\"value\": \"51.0\"},");
 		chartData = chartData.concat("{\"label\": \"11/15\",\"value\": \"65.0\"},");
 		chartData = chartData.concat("{\"label\": \"12/15\",\"value\": \"55.0\"},");
@@ -51,13 +51,13 @@ public class OrgRiskHistoryLineChart {
 		chartData = chartData.concat("{\"label\": \"6/16\",\"value\": \"35.0\"},");
 		chartData = chartData.concat("{\"label\": \"7/16\",\"value\": \"30.0\"},");
 		chartData = chartData.concat("{\"label\": \"8/16\",\"value\": \"45.0\"},");
-		chartData = chartData.concat("{\"label\": \"9/16\",\"value\": \"82.0\",\"toolText\": \"Global Security Event: Petya\"},");
+		chartData = chartData.concat("{\"label\": \"9/16\",\"value\": \"84.0\",\"toolText\": \"Global Security Event: Petya\"},");
 		chartData = chartData.concat("{\"label\": \"10/16\",\"value\":\"40.0\",\"toolText\": \"Petya Mitigation\"},");
 		chartData = chartData.concat("{\"label\": \"11/16\",\"value\": \"35.0\"},");
 		chartData = chartData.concat("{\"label\": \"12/16\",\"value\": \"25.0\"},");
 		chartData = chartData.concat("{\"label\": \"1/17\",\"value\": \"20.0\"},");
 		chartData = chartData.concat("{\"label\": \"2/17\",\"value\": \"15.0\"},");
-		chartData = chartData.concat("{\"label\": \"3/17\",\"value\": \"85.0\",\"toolText\": \"Global Security Event: WannaCry\"},");
+		chartData = chartData.concat("{\"label\": \"3/17\",\"value\": \"86.0\",\"toolText\": \"Global Security Event: WannaCry\"},");
 		chartData = chartData.concat("{\"label\": \"4/17\",\"value\": \"30.0\",\"toolText\": \"WannaCry Mitigation\"},");
 		chartData = chartData.concat("{\"label\": \"5/17\",\"value\": \"25.0\"},");
 		chartData = chartData.concat("{\"label\": \"6/17\",\"value\": \"22.0\"},");
@@ -65,10 +65,10 @@ public class OrgRiskHistoryLineChart {
 		chartData = chartData.concat("],");
 		chartData = chartData.concat("\"trendlines\": [{");
 		chartData = chartData.concat("\"line\": [{");
-		chartData = chartData.concat("\"startvalue\": \"65.0\",");
-		chartData = chartData.concat("\"endvalue\": \"65.00\",");
+		chartData = chartData.concat("\"startvalue\": \"35.0\",");
+		chartData = chartData.concat("\"endvalue\": \"35.00\",");
 		chartData = chartData.concat("\"color\": \"#595a5c\",");
-		chartData = chartData.concat("\"displayvalue\": \"Mission{br}Effectiveness{br}Goal 65%\",");
+		chartData = chartData.concat("\"displayvalue\": \"Mission{br}Impact{br}Threshold 35%\",");
 		chartData = chartData.concat("\"valueOnRight\": \"1\",");
 		chartData = chartData.concat("\"thickness\": \"2\"");
 		chartData = chartData.concat("}]");
