3071b02ad40b33b7cf9912010960bc7e25b9f617
John Reddy <john.reddy@g2-ops.com>
2017-12-17 22:08:32 -0500

    SessionUtils Utility Class

---
+++ URM/src/main/java/com/g2ops/impact/urm/utils/SessionUtils.java
@@ -0,0 +1,92 @@
+package com.g2ops.impact.urm.utils;
+
+import com.datastax.driver.core.Session;
+import com.g2ops.impact.urm.utils.DatabaseConnectionManager;
+import java.util.Map;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class SessionUtils {
+
+	public static HttpSession getSession() {
+		return (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+	}
+
+	public static HttpServletRequest getRequest() {
+		return (HttpServletRequest) FacesContext.getCurrentInstance()
+				.getExternalContext().getRequest();
+	}
+
+	public static Session getOrgDBSession() {
+
+		// get the servlet's context
+		ServletContext ctx = getRequest().getServletContext();
+
+		// get the user's session
+		HttpSession userSession = getSession();
+
+		// get the orgKeyspace from the session
+		String orgKeyspace = (String)userSession.getAttribute("orgKeyspace");
+
+		// get the Hash Map of the Organization database connections
+		@SuppressWarnings("unchecked")
+		Map<String, DatabaseConnectionManager> DBConnectionsHashMap = (Map<String, DatabaseConnectionManager>) ctx.getAttribute("OrgDBConnections");
+		
+		// get the database connection for this user's organization
+		DatabaseConnectionManager orgDBConnection = DBConnectionsHashMap.get(orgKeyspace);
+		
+		// get the database connection session
+		Session DBSession = orgDBConnection.getSession();
+		
+		// return the Org's DB Session
+		return DBSession;
+
+	}
+
+	public static DatabaseQueryService getOrgDBQueryService() {
+
+		// get the servlet's context
+		ServletContext ctx = getRequest().getServletContext();
+
+		// get the user's session
+		HttpSession userSession = getSession();
+
+		// get the orgKeyspace from the session
+		//String orgKeyspace = (String)userSession.getAttribute("orgKeyspace");
+
+		// get the Hash Map of the Organization database query services
+		@SuppressWarnings("unchecked")
+		Map<String, DatabaseQueryService> DBQueryServicesHashMap = (Map<String, DatabaseQueryService>) ctx.getAttribute("OrgDBQueryServices");
+		
+		// get the database query service for this user's organization
+		DatabaseQueryService orgDBQueryService = DBQueryServicesHashMap.get((String)userSession.getAttribute("orgKeyspace"));
+		
+		// return the Org's DB Query Service
+		return orgDBQueryService;
+
+	}
+
+	/*
+	public static String getOrgKeyspace() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+		return session.getAttribute("orgKeyspace").toString();
+	}
+
+	public static String getUserEmail() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+		return session.getAttribute("userEmail").toString();
+	}
+
+	public static String getUserRole() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+		return session.getAttribute("userRole").toString();
+	}
+	*/
+	
+}
