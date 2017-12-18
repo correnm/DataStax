4df76aee952f4594cfb3839d2f84b128bd3061e5
John Reddy <john.reddy@g2-ops.com>
2017-12-17 21:49:24 -0500

    BusinessApplication Type Class

---
+++ URM/src/main/java/com/g2ops/impact/urm/types/BusinessApplication.java
@@ -0,0 +1,52 @@
+package com.g2ops.impact.urm.types;
+
+public class BusinessApplication {
+		
+	private String appName;
+	private String recordType;
+	private Integer numRecords;
+	private Integer breachCost;
+	private Integer dw;
+	private String nivScore;
+	private Integer cyvarValue;
+
+	public BusinessApplication (String appName, String recordType, Integer numRecords, Integer breachCost, Integer dw, String nivScore, Integer cyvarValue) {
+		this.appName = appName;
+		this.recordType = recordType;
+		this.numRecords = numRecords;
+		this.breachCost = breachCost;
+		this.dw = dw;
+		this.nivScore = nivScore;
+		this.cyvarValue = cyvarValue;
+	}
+	
+	public String getAppName() {
+		return appName;
+	}
+		
+	public String getRecordType() {
+		return recordType;
+	}
+		
+	public Integer getNumRecords() {
+		return numRecords;
+	}
+		
+	public Integer getBreachCost() {
+		return breachCost;
+	}
+		
+	public Integer getDw() {
+		return dw;
+	}
+		
+	public String getNivScore() {
+		return nivScore;
+	}
+		
+	public Integer getCyvarValue() {
+		return cyvarValue;
+	}
+		
+}
+
