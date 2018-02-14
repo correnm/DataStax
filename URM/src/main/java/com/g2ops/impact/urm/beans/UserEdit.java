974aabebf32f970581e74b4e5f5c7e4c078942a0
John Reddy <john.reddy@g2-ops.com>
2018-02-13 23:06:55 -0500

    CDI Bean Update

--- URM/src/main/java/com/g2ops/impact/urm/beans/UserEdit.java
+++ URM/src/main/java/com/g2ops/impact/urm/beans/UserEdit.java
@@ -14,9 +14,14 @@ package com.g2ops.impact.urm.beans;
  * 
  */
 
-import javax.enterprise.context.RequestScoped;
+import javax.annotation.PostConstruct;
+
+import javax.enterprise.context.SessionScoped;
+
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 
 import java.security.NoSuchAlgorithmException;
@@ -25,28 +30,29 @@ import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 
 import com.g2ops.impact.urm.types.OrgUnit;
 import com.g2ops.impact.urm.types.Site;
-import com.g2ops.impact.urm.types.User;
 import com.g2ops.impact.urm.utils.DatabaseQueryService;
 import com.g2ops.impact.urm.utils.PasscodeEncryptionService;
 import com.g2ops.impact.urm.utils.SessionUtils;
 
 @Named("userEdit")
-@RequestScoped
-public class UserEdit {
+@SessionScoped
+public class UserEdit implements Serializable {
 
-	//private Row row;
-	//private Iterator<Row> iterator;
-	private String userEmail, newPasscode, firstName, lastName, role, selectedOrgUnit, selectedSite, org_id;
+	private static final long serialVersionUID = 1L;
+
+	@Inject private UserBean currentUser;
+
+	private DatabaseQueryService databaseQueryService;
+	private ResultSet rs;
+	private Iterator<Row> iterator;
+	private Row row;
+	private String userToEditEmail, newPasscode, firstName, lastName, role, selectedOrgUnit, selectedSite, org_id;
 	//private String orgUnitID, siteID, defaultLensView;
 	//private Boolean systemAdministratorInd;
 	private Integer iterations = 20000;
@@ -56,30 +62,17 @@ public class UserEdit {
 	private List<Site> sites;
 
 	// constructor
-	public UserEdit() throws NoSuchAlgorithmException, InvalidKeySpecException {
+	public UserEdit() {
 
-		Row row;
-		Iterator<Row> iterator;
-		
-		// get the user's session
-		HttpSession userSession = SessionUtils.getSession();
-		
+	}
+	
+	@PostConstruct
+	public void init() {
+
+		System.out.println("in UserEdit init method");
+
 		// get the Database Query Service object for this Org
-		DatabaseQueryService databaseQueryService = SessionUtils.getOrgDBQueryService();
-		
-		// get the default OU and Site from the user's session
-		selectedOrgUnit = (String)userSession.getAttribute("currentOU");
-		selectedSite = (String)userSession.getAttribute("currentSite");
-		System.out.println("selectedOrgUnit: " + selectedOrgUnit + " | selectedSite: " + selectedSite);
-
-		// get the org_id for using in queries (temporary thing)
-		String orgKeyspace = (String)userSession.getAttribute("orgKeyspace");
-		if (orgKeyspace.equals("g2")) {
-			org_id = "df72e1dd-a385-4ab9-ba30-70b9df8d539b";
-		}
-		else if (orgKeyspace.equals("vmasc")) {
-			org_id = "2cd3bc78-350f-47ce-bd53-7525b93f0640";
-		}
+		databaseQueryService = SessionUtils.getOrgDBQueryService(currentUser.getOrgKeyspace());
 
 		// get List of all possible roles
 		this.roles = new ArrayList<String>();
@@ -87,13 +80,12 @@ public class UserEdit {
 		this.roles.add("user");
 
 		// get List of all possible OUs
-		ResultSet rs = databaseQueryService.runQuery("select org_unit_id, org_unit_name from organizational_units");
+		rs = databaseQueryService.runQuery("select org_unit_id, org_unit_name from organizational_units");
 		iterator = rs.iterator();
 		
 		this.orgUnits = new ArrayList<OrgUnit>();
 		while (iterator.hasNext()) {
 			row = iterator.next();
-			System.out.println("OU Query Row: " + row);
 			this.orgUnits.add(new OrgUnit(row.getUUID("org_unit_id").toString(), row.getString("org_unit_name")));
 			System.out.println("OU List: " + this.orgUnits);
 		}
@@ -105,32 +97,32 @@ public class UserEdit {
 		this.sites.clear();
 		
 		// execute the query
-		rs = databaseQueryService.runQuery("select site_id, site_name from sites where org_id = " + UUID.fromString(org_id) + " and org_unit_id = " + UUID.fromString(selectedOrgUnit));
+		//rs = databaseQueryService.runQuery("select site_id, site_name from sites where org_unit_id = " + UUID.fromString(selectedOrgUnit));
 
-		iterator = rs.iterator();
+		//iterator = rs.iterator();
 		
-		while (iterator.hasNext()) {
-			row = iterator.next();
-			System.out.println("Site Query Row: " + row);
-			this.sites.add(new Site(row.getUUID("site_id").toString(), row.getString("site_name")));
-		}
+		//while (iterator.hasNext()) {
+			//row = iterator.next();
+			//System.out.println("Site Query Row: " + row);
+			//this.sites.add(new Site(row.getUUID("site_id").toString(), row.getString("site_name")));
+		//}
 
 
 		// get List of all possible Default Pages
-		
 
-		// get the email address of the user to edit from the URL parameter
-		HttpServletRequest request = SessionUtils.getRequest();
-		userEmail = request.getParameter("userEmail");
-		
+	}
+	
+	public void retrieveUserData() {
+
+		System.out.println("userToEditEmail: " + userToEditEmail);
+
 		// select the user's data
-		rs = databaseQueryService.runQuery("select user_email, application_role_name, org_unit_id, site_id, default_lens_view_r, first_name, last_name, system_administrator_ind from users where user_email = '" + userEmail + "'");
+		rs = databaseQueryService.runQuery("select user_email, application_role_name, org_unit_id, site_id, default_lens_view_r, first_name, last_name, system_administrator_ind from users where user_email = '" + userToEditEmail + "'");
 
 		// get the result record
 		row = rs.one();
-
+		
 		// set values to what was returned by the query
-		userEmail = row.getString("user_email");
 		firstName = row.getString("first_name");
 		lastName = row.getString("last_name");
 		role = row.getString("application_role_name");
@@ -141,8 +133,8 @@ public class UserEdit {
 		
 	}
 
-    public String getUserEmail() {
-    	return userEmail;
+    public String getUserToEditEmail() {
+    	return userToEditEmail;
     }
 
     public String getNewPasscode() {
@@ -165,6 +157,11 @@ public class UserEdit {
 		return roles;
 	}
 
+    public void setUserToEditEmail(String userToEditEmail) {
+		System.out.println("in UserEdit setUserToEditEmail method");
+    	this.userToEditEmail = userToEditEmail;
+    }
+
     public void setNewPasscode(String newPasscode) {
     	this.newPasscode = newPasscode;
     }
@@ -183,16 +180,10 @@ public class UserEdit {
 
 	public String editUserActionControllerMethod() throws NoSuchAlgorithmException, InvalidKeySpecException, UnsupportedEncodingException {
 
-		//System.out.println("*** editUserActionControllerMethod ***");
+		System.out.println("*** BEGIN editUserActionControllerMethod ***");
 
-		// get the user's session
-		HttpSession userSession = SessionUtils.getSession();
-
-		// get the User instance from the user's session
-		User user = (User) userSession.getAttribute("user");
-		
 		// get the Database Query Service object for this Org
-		DatabaseQueryService databaseQueryService = SessionUtils.getOrgDBQueryService();
+		DatabaseQueryService databaseQueryService = SessionUtils.getOrgDBQueryService(currentUser.getOrgKeyspace());
 		
 		// if setting a new password
 		if (!newPasscode.equals("")) {
@@ -220,14 +211,14 @@ public class UserEdit {
 			passcodeValueToStore = passcodeValueToStore.concat(encryptedPasscodeString);
     	
 			// execute the query to update the passcode in the database
-			databaseQueryService.runUpdateQuery("update users set hashed_password = '" + passcodeValueToStore + "' where user_email = '" + userEmail + "'");
-
-			//System.out.println("*** editUserActionControllerMethod ***");
+			databaseQueryService.runUpdateQuery("update users set hashed_password = '" + passcodeValueToStore + "' where user_email = '" + userToEditEmail + "'");
 
 		}
 
 		// update the user's info in the Users table for the user that was edited
-		databaseQueryService.runUpdateQuery("update users set first_name = '" + this.firstName + "', last_name = '" + this.lastName + "', application_role_name = '" + this.role + "', audit_upsert = { datechanged : dateof(now()), changedbyusername : '" + user.getUserEmail() + "' } where user_email = '" + userEmail + "'");
+		databaseQueryService.runUpdateQuery("update users set first_name = '" + this.firstName + "', last_name = '" + this.lastName + "', application_role_name = '" + this.role + "', audit_upsert = { datechanged : dateof(now()), changedbyusername : '" + currentUser.getEmail() + "' } where user_email = '" + userToEditEmail + "'");
+
+		System.out.println("*** END editUserActionControllerMethod ***");
 
 		return "users-table";
 
