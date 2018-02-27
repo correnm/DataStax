6d810b0906e4aac83e6a6880271274852d1e57be
John Reddy <john.reddy@g2-ops.com>
2018-02-26 23:02:57 -0500

    Massive Changes

--- URM/src/main/java/com/g2ops/impact/urm/beans/UsersTable.java
+++ URM/src/main/java/com/g2ops/impact/urm/beans/UsersTable.java
@@ -25,6 +25,7 @@ import java.util.List;
 
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
+import com.g2ops.impact.urm.types.NavMenuItem;
 import com.g2ops.impact.urm.types.User;
 import com.g2ops.impact.urm.utils.DatabaseQueryService;
 import com.g2ops.impact.urm.utils.SessionUtils;
@@ -34,8 +35,16 @@ import com.g2ops.impact.urm.utils.SessionUtils;
 public class UsersTable {
 
 	@Inject private UserBean currentUser;
+	@Inject private NavMenu dashboardsNavMenu;
+
+	private DatabaseQueryService databaseQueryService;
+	private ResultSet rs, rs2, rs3;
+	private Row row, row2, row3;
+
+	String defaultDashboardDisplayName = "";
 
 	private List<User> userList = new ArrayList<User>();
+	private List<NavMenuItem> dashboardsList = new ArrayList<NavMenuItem>();
 
 	public UsersTable() {
 		
@@ -44,17 +53,37 @@ public class UsersTable {
 	@PostConstruct
 	public void init() {
 
+		// get List of all Dashboards
+		dashboardsList = dashboardsNavMenu.getDashboardsNavMenuItemList();
+
 		// get the Database Query Service object for this Org
-		DatabaseQueryService databaseQueryService = SessionUtils.getOrgDBQueryService(currentUser.getOrgKeyspace());
+		databaseQueryService = SessionUtils.getOrgDBQueryService(currentUser.getOrgKeyspace());
 		
 		// execute the query
-		ResultSet rs = databaseQueryService.runQuery("select user_email, application_role_name, default_lens_view_r, first_name, last_name, system_administrator_ind from users");
+		rs = databaseQueryService.runQuery("select user_email, application_role_name, default_lens_view_r, org_unit_id, site_id, first_name, last_name, active_user_ind from users");
 
 		Iterator<Row> iterator = rs.iterator();
 		
+		// iterate through each row (user record)
 		while (iterator.hasNext()) {
-			Row row = iterator.next();
-			User user = new User(row.getString("user_email"), row.getString("first_name"), row.getString("last_name"), row.getString("application_role_name"), row.getString("default_lens_view_r"), row.getBool("system_administrator_ind"));
+
+			// grab the next row
+			row = iterator.next();
+
+			// obtain the display name of the user's default dashboard
+			for (NavMenuItem dashboardNavItem : dashboardsList) {
+				if (dashboardNavItem.getFileName().equals(row.getString("default_lens_view_r"))) {
+					defaultDashboardDisplayName = dashboardNavItem.getDisplayName();
+				}
+			}
+
+			rs2 = databaseQueryService.runQuery("select org_unit_name from organizational_units where org_unit_id = " + row.getUUID("org_unit_id"));
+			row2 = rs2.one();
+			
+			rs3 = databaseQueryService.runQuery("select site_name from sites where org_unit_id = " + row.getUUID("org_unit_id") + " and site_id = " + row.getUUID("site_id"));
+			row3 = rs3.one();
+			
+			User user = new User(row.getString("user_email"), row.getString("first_name"), row.getString("last_name"), row.getString("application_role_name"), defaultDashboardDisplayName, row2.getString("org_unit_name"), row3.getString("site_name"), row.getBool("active_user_ind"));
 			userList.add(user);
 		}
 		
@@ -66,8 +95,4 @@ public class UsersTable {
 		
 	}
 
-	public String editUser() {
-		return "/administrator/user-edit.jsf";
-	}
-	
 }
